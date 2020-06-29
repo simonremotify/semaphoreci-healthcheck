af2074f50ab2e6c763fd4ac4039d717f09f07f09
Simon Deeley <simon@remotify.work>
2020-06-29 14:35:04 +0100

    Check that google.com is up as an example healthcheck

--- run_healthcheck.sh
+++ run_healthcheck.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-# Stub: always fail for now
-exit 1
+ENDPOINT=https://www.google.com
 
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null $ENDPOINT)
+
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "$ENDPOINT status $status_code"
+  exit 1
+else
+  echo "$ENDPOINT OK"
+  exit 0
+fi
